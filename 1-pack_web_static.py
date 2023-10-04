3a16a96d0e9adac23726200b0577daf02264d24a
2Kelvin <rocketmancodes02@gmail.com>
2023-10-04 07:37:25 -0700

    compress web static files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+"""Compressing before sending"""
+from fabric.api import local
+from time import strftime
+
+
+def do_pack():
+    """Compress web_static folder to tgz format"""
+    local('mkdir versions')
+    compressedFile = f'web_static_{strftime("%Y%m%d%H%M%S")}'
+    filePath = f'versions/{compressedFile}.tgz'
+    print(f'Packing web_static to {filePath}')
+    cmd_tgz = local(f'tar -czvf {filePath} web_static')
+    if cmd_tgz.succeeded:
+        return filePath
+    else:
+        return None
