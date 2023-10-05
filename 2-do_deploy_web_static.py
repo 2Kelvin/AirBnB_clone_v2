d440066d7d2586efdfdeb37d5d15666d7f690c7f
2Kelvin <rocketmancodes02@gmail.com>
2023-10-05 01:18:58 -0700

    error fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 """Deploy archive!"""
-from fabric.api import put, run, env, sudo
+from fabric.api import put, run, env
 from os import path
 
 
 env.hosts = ['54.237.88.98', '35.153.67.97']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+# env.user = 'ubuntu'
+# env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
     """Deploys compressed web static file to my web servers"""
-    if not path.exists(archive_path):
+    if not path.exists(archive_path) or path.isfile(archive_path) is False:
         return False
     put(archive_path, '/tmp/')
-
-    archiveName = archive_path.split("/")[-1].split(".")[0]
-    newStaticArchivePath = f'/data/web_static/releases/{archiveName}'
-    sudo(f'tar -xzf /tmp/{archiveName} -C {newStaticArchivePath}')
-    sudo(f'rm -r /tmp/{archive_path}')
+    fileName = archive_path.split("/")[-1].split(".")[0]
+    webStaticFilePath = f'/data/web_static/releases/{fileName}'
+    run(f'sudo tar -xzf /tmp/{archive_path} -C {webStaticFilePath}')
+    run(f'sudo rm -r /tmp/{archive_path}')
 
     symbolicLink = '/data/web_static/current'
-    sudo(f'rm -rf {symbolicLink}')
-    sudo(f'ln -s {newStaticArchivePath} {symbolicLink}')
+    run(f'sudo rm -rf {symbolicLink}')
+    run(f'sudo ln -s {webStaticFilePath} {symbolicLink}')
     return True
