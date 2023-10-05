6bcd27961c296f522269e023e0ab53aeff1a87dc
2Kelvin <rocketmancodes02@gmail.com>
2023-10-05 03:23:38 -0700

    fix output errs

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,27 +5,27 @@ from os import path
 
 
 env.hosts = ['54.237.88.98', '35.153.67.97']
-# env.user = 'ubuntu'
-# env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
     """Deploys compressed web static file to my web servers"""
     if not path.exists(archive_path) or path.isfile(archive_path) is False:
         return False
-    put(archive_path, '/tmp/')
     archfile = archive_path.split('/')[-1]
     archfileNotgz = archive_path.split('/')[-1].split('.')[0]
     # dirVersions = archive_path.split("/")[0]
     webStaticFilePath = f'/data/web_static/releases/{archfileNotgz}/'
-    run(f'sudo mkdir -p {webStaticFilePath}')
-    run(f'sudo tar -xzf /tmp/{archfile} -C {webStaticFilePath}')
 
-    run(f'sudo rm -r /tmp/{archfile}')
-    run(f'sudo mv {webStaticFilePath}web_static/* {webStaticFilePath}')
-    run(f'sudo rm -rf {webStaticFilePath}web_static')
+    put(archive_path, '/tmp/')
+    run(f'mkdir -p {webStaticFilePath}')
+    run(f'tar -xzf /tmp/{archfile} -C {webStaticFilePath}')
+
+    run(f'rm /tmp/{archfile}')
+    run(f'mv {webStaticFilePath}web_static/* {webStaticFilePath}')
+    run(f'rm -rf {webStaticFilePath}web_static')
 
     symbolicLink = '/data/web_static/current'
-    run(f'sudo rm -rf {symbolicLink}')
-    run(f'sudo ln -s {webStaticFilePath} {symbolicLink}')
+    run(f'rm -rf {symbolicLink}')
+    run(f'ln -s {webStaticFilePath} {symbolicLink}')
+    print('New version deployed!')
     return True
