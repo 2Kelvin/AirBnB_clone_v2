12ff2ac55ec467eabf533819dd4ae02f8dbbf09f
2Kelvin <rocketmancodes02@gmail.com>
2023-10-05 01:40:08 -0700

    fix rm tmp dir

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,11 +15,11 @@ def do_deploy(archive_path):
         return False
     put(archive_path, '/tmp/')
     archfile = archive_path.split('/')[-1]
-    fileName = archive_path.split("/")[-1].split(".")[0]
+    dirVersions = archive_path.split("/")[0]
     # run('sudo mkdir -p /data/web_static/releases')
     webStaticFilePath = '/data/web_static/releases/'
     run(f'sudo tar -xzf /tmp/{archfile} -C {webStaticFilePath}')
-    run(f'sudo rm -r /tmp/{archive_path}')
+    run(f'sudo rm -r /tmp/{dirVersions}')
 
     symbolicLink = '/data/web_static/current'
     run(f'sudo rm -rf {symbolicLink}')
