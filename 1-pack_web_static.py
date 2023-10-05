e0179a0ad2dedadb2cc915cf2e16f04afeb14592
2Kelvin <rocketmancodes02@gmail.com>
2023-10-04 22:07:29 -0700

    debug #5

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,14 +2,12 @@
 """Compressing before sending"""
 from fabric.api import local
 from time import strftime
-from datetime import datetime
 
 
 def do_pack():
     """Compress web_static folder to tgz format"""
-    myTime = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('mkdir versions')
-    compressedFile = f'web_static_{myTime}'
+    local('mkdir -p versions')
+    compressedFile = f'web_static_{strftime("%Y%m%d%H%M%S")}'
     filePath = f'versions/{compressedFile}.tgz'
     cmd_tgz = local(f'tar -czvf {filePath} web_static')
     if cmd_tgz.succeeded:
