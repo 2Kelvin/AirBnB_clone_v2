1a290534ef3469489d1e234dced43c45b83ae3ea
2Kelvin <rocketmancodes02@gmail.com>
2023-10-04 23:51:21 -0700

    use sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,8 @@ from os import path
 
 
 env.hosts = ['54.237.88.98', '35.153.67.97']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+# env.user = 'ubuntu'
+# env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
@@ -15,9 +15,9 @@ def do_deploy(archive_path):
         return False
     put(archive_path, '/tmp/')
     archiveName = archive_path.split("/")[-1].split(".")[0]
-    run(f'tar -xzf /tmp/{archiveName} -C /data/web_static/releases/')
-    run(f'rm -r /tmp/{archive_path}')
-    run('rm -rf /data/web_static/current')
-    run(f'ln -s /data/web_static/releases/{archiveName} \
+    run(f'sudo tar -xzf /tmp/{archiveName} -C /data/web_static/releases/')
+    run(f'sudo rm -r /tmp/{archive_path}')
+    run('sudo rm -rf /data/web_static/current')
+    run(f'sudo ln -s /data/web_static/releases/{archiveName} \
         /data/web_static/current')
     return True
