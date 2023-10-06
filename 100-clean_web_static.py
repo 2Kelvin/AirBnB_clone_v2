40428a5aec250459ae1ad99c8371eadae19d3012
2Kelvin <rocketmancodes02@gmail.com>
2023-10-06 03:25:22 -0700

    try try try

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
 """Keep it clean!"""
-from fabric.api import run, lcd, local, cd, env
-from os import listdir
+from fabric.api import run,  local, env
 
 env.hosts = ['54.237.88.98', '35.153.67.97']
 
 
+def cleanSandbox(number=0):
+    """Clean sandbox archives"""
+    arrayArchives = local('ls -1t versions', capture=True)
+    arrayArchives = arrayArchives.split('\n')
+    num = int(number)
+    if num is 0 or num is 1:
+        num = 1
+    print(len(arrayArchives[num:]))
+    for n in arrayArchives[num:]:
+        local('rm versions/' + n)
+
+
+def cleanServers(number=0):
+    """Clean server archives"""
+    arrayArchives = run('ls -1t /data/web_static/releases')
+    arrayArchives = arrayArchives.split('\r\n')
+    print(arrayArchives)
+    num = int(number)
+    if num is 0 or num is 1:
+        num = 1
+    print(len(arrayArchives[num:]))
+    for x in arrayArchives[num:]:
+        if x is not 'test':
+            run('rm -rf /data/web_static/releases/' + x)
+
+
 def do_clean(number=0):
     """Deletes out of date archives"""
-    if int(number) == 0:
-        number = 1
-    else:
-        int(number)
-    allArchives = sorted(listdir('versions'))
-    for x in range(number):
-        allArchives.pop()
-    with lcd('versions'):
-        for eachArchve in allArchives:
-            local('rm ./{}'.format(eachArchve))
-    with cd('/data/web_static/releases'):
-        allArchives = run('ls -tr').split()
-        allArchives = []
-        for arch in allArchives:
-            if 'web_static_' in arch:
-                allArchives.append(arch)
-        for a in range(number):
-            allArchives.pop()
-        for x in allArchives:
-            run('rm -rf ./{}'.format(x))
+    cleanSandbox(number)
+    cleanServers(number)
