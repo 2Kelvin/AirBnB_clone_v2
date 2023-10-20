7436845457574e4981d0bafec62bd4e2f116fc4a
2Kelvin <rocketmancodes02@gmail.com>
2023-10-20 07:18:13 -0700

    add tasks 3 and 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module containing route python"""
+from flask import Flask
+from markupsafe import escape
+
+flaskApp = Flask(__name__)
+
+
+@flaskApp.route('/', strict_slashes=False)
+def helloHBNB():
+    """Display hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@flaskApp.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return 'HBNB'
+
+
+@flaskApp.route('/c/<text>', strict_slashes=False)
+def cIsFun(text):
+    """Displays route c"""
+    spaceText = text.replace('_', ' ')
+    return 'C {}'.format(escape(spaceText))
+
+
+@flaskApp.route('/python/<text>', strict_slashes=False)
+@flaskApp.route('/python', strict_slashes=False)
+def pythonIsCool(text='is cool'):
+    """Displays route python"""
+    if text: text = text.replace('_', ' ')
+    return 'Python {}'.format(escape(text))
+
+
+@flaskApp.route('/number/<int:n>', strict_slashes=False)
+def numberN(n):
+    """Display only numbers"""
+    return '{} is an integer'.format(n)
+
+if __name__ == '__main__':
+    flaskApp.run(host='0.0.0.0', port=5000)
+
