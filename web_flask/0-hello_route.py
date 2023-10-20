fa27ee9b4fe6279a0bb273a66053e41a58e185e8
2Kelvin <rocketmancodes02@gmail.com>
2023-10-20 04:16:46 -0700

    change flaskApp to app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,14 +2,14 @@
 """Module containing flask app"""
 from flask import Flask
 
-flaskApp = Flask(__name__)
+app = Flask(__name__)
 
 
-@flaskApp.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def helloHBNB():
     """Display hello HBNB"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
-if __name__ == "__main__":
-    flaskApp.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
