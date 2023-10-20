e6478d536c3926e8631af0ae427f5f3c3ecf3c94
2Kelvin <rocketmancodes02@gmail.com>
2023-10-20 04:20:12 -0700

    add space after module doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Module containing flask app"""
+
 from flask import Flask
 
-app = Flask(__name__)
+flaskApp = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@flaskApp.route('/', strict_slashes=False)
 def helloHBNB():
     """Display hello HBNB"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    flaskApp.run(host='0.0.0.0', port=5000)
 
