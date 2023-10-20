217d0d587468c058394706958f0f66475b724539
2Kelvin <rocketmancodes02@gmail.com>
2023-10-20 04:34:47 -0700

    try this punk!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Module containing flask app"""
+"""Module containing route hbnb"""
 from flask import Flask
 
-flaskApp = Flask(__name__)
+flaskApp = Flask('__name__')
 
 
-@flaskApp.route('/', strict_slashes=False)
+@flaskApp.route('/',strict_slashes=False)
 def helloHBNB():
     """Display hello HBNB"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    flaskApp.run(host='0.0.0.0', port=5000)
+    flaskApp.run(host='0.0.0.0',port=5000)
 
