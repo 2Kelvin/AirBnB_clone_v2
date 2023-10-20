2c2fa7db3e4741f55140145cbdf27de5b3fc7ed0
2Kelvin <rocketmancodes02@gmail.com>
2023-10-20 04:28:10 -0700

    add brackets

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Module containing flask app"""
-
 from flask import Flask
 
 flaskApp = Flask(__name__)
@@ -9,7 +8,7 @@ flaskApp = Flask(__name__)
 @flaskApp.route('/', strict_slashes=False)
 def helloHBNB():
     """Display hello HBNB"""
-    return 'Hello HBNB!'
+    return ('Hello HBNB!')
 
 if __name__ == '__main__':
     flaskApp.run(host='0.0.0.0', port=5000)
