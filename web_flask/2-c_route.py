60bb3f30606efafcdcd866b68a8b82b71b690428
2Kelvin <rocketmancodes02@gmail.com>
2023-10-20 05:07:41 -0700

    use variables in url

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module containing route c"""
+from flask import Flask
+from markupsafe import escape
+
+flaskApp = Flask(__name__)
+
+
+@flaskApp.route('/', strict_slashes=False)
+def helloHBNB():
+    """Display hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@flaskApp.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return 'HBNB'
+
+
+@flaskApp.route('/c/<text>', strict_slashes=False)
+def cIsFun(text):
+    """Displays route c"""
+    spaceText = text.replace('_', ' ')
+    return 'C {}'.format(escape(spaceText))
+
+if __name__ == '__main__':
+    flaskApp.run(host='0.0.0.0', port=5000)
+
