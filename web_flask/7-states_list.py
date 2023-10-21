7131b7b49c5d71a21a4f7e9e503bfcf72c955517
2Kelvin <rocketmancodes02@gmail.com>
2023-10-21 04:24:55 -0700

    add task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module contains list of states template rendered by Flask"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+flaskApp = Flask(__name__)
+
+
+@flaskApp.teardown_appcontext
+def teardownSession():
+    """Teardown method to remove the SQLAlchemy session"""
+    storage.close()
+
+
+@flaskApp.route('/states_list', strict_slashes=False)
+def statesList():
+    """Route to render states list"""
+    allStates = storage.all(State)
+    return render_template('7-states_list.html', allStates)
+
+
+if __name__ == '__main__':
+    flaskApp.run(host='0.0.0.0', port=5000)
