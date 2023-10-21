95dc401b28acf7356b089a9f6b4afe36199596a4
2Kelvin <rocketmancodes02@gmail.com>
2023-10-21 05:27:17 -0700

    render cities baaby

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module contains a cities template rendered by Flask"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+flaskApp = Flask(__name__)
+
+
+@flaskApp.teardown_appcontext
+def teardownSession(self):
+    """Teardown method to remove the SQLAlchemy session"""
+    storage.close()
+
+
+@flaskApp.route('/cities_by_states', strict_slashes=False)
+def statesList():
+    """Route to render states list"""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', allStates=states)
+
+
+if __name__ == '__main__':
+    flaskApp.run(host='0.0.0.0', port=5000)
