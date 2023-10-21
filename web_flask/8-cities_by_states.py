26eca0e0df1f7065edf5e0a34a1073763292fe7e
2Kelvin <rocketmancodes02@gmail.com>
2023-10-21 13:10:26 -0700

    it didnt work

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,7 +3,6 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 
 flaskApp = Flask(__name__)
@@ -18,13 +17,8 @@ def teardownSession(self):
 @flaskApp.route('/cities_by_states', strict_slashes=False)
 def citiesDisplayed():
     """Route to render states list"""
-    allStates = storage.all(State).values()
-    allCities = storage.all(City).values()
-    return render_template(
-        '8-cities_by_states.html',
-        states=allStates,
-        cities=allCities
-    )
+    dataStates = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=dataStates)
 
 
 if __name__ == '__main__':
